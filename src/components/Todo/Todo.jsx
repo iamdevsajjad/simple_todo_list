f8ad9afd6ed815f64fcd490e158067e8824d7f5e
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-11-08 22:30:05 +0600

    complete basic website

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,32 @@
-import React from "react";
-import { AiOutlinePlus } from 'react-icons/ai';
+import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { FiTrash2 } from "react-icons/fi";
 import image from "../../assets/todo.png";
+
 const Todo = () => {
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState("");
+
+  const addItem = (e) => {
+    e.preventDefault();
+    if (!inputData) {
+      setError("Please write your todo");
+    } else {
+      setItems([...items, inputData]);
+      setError("");
+      setInputData("")
+    }
+  };
+
+  const deleteItem = (id) => {
+    console.log(id);
+    const updatedItems = items.filter((elem, index) => id !== index);
+    setItems(updatedItems)
+  }
+
+  console.log(items);
+
   return (
     <div className="fullTodo bg-blue-900 min-h-screen">
       <div className="todo  w-80 mx-auto p-5">
@@ -9,14 +34,40 @@ const Todo = () => {
           <img className="w-1/2 mx-auto" src={image} alt="" />
         </div>
         <div className="texts text-white text-center">
-          <h1 className="text-xl font-sans">Add Your List Here</h1>
+          <h1 className="text-xl font-sans">Add Your List Here ✌️</h1>
         </div>
-        <div className="input my-4 relative">
-          <input className="w-full p-3 rounded-sm my-2  outline-none " placeholder="✍️ add something ..." type="text" />
-          <button className="absolute top-5 right-3 text-2xl hover:text-green-500 transition-all text-black"><AiOutlinePlus /></button>
+        <form onSubmit={addItem} className="input my-4 relative">
+          <input
+            className="w-full p-3 rounded-sm outline-none "
+            placeholder="✍️ add something ..."
+            type="text"
+            onChange={(e) => setInputData(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="absolute top-3 right-3 text-2xl hover:text-green-500 transition-all text-black"
+          >
+            <AiOutlinePlus />
+          </button>
+          {error && <small className="text-red-400">{error}</small>}
+        </form>
+        <div className="mb-3 space-y-1">
+          {items.map((item, index) => (
+            <div
+              className="bg-indigo-500 text-white px-2 py-2 rounded flex justify-between"
+              key={index + 1}
+            >
+              <p>{item}</p>
+              <button
+              onClick={() => deleteItem(index)}
+              className="p-1 text-lg">
+                <FiTrash2 />
+              </button>
+            </div>
+          ))}
         </div>
         <div className="buttonDiv  flex justify-center ">
-          <button className="bg-white text-black py-1 rounded-sm px-2 ">
+          <button className="bg-white text-black py-1 rounded-sm px-2 hover:bg-black hover:text-white transition-all">
             Check List
           </button>
         </div>
